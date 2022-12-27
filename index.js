f92fffe10d92061c9c9b0d6b817ee34ce16517b7
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-12-27 13:28:50 -0700

    added /kb

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const { v4: uuid } = require('uuid');
 const sgMail = require('@sendgrid/mail');
+const path = require('path');
 
-const whitelist = ['https://itbsutah.com', 'https://support.itbsutah.com']
+const whitelist = ['https://itbsutah.com', 'https://support.itbsutah.com', 'https://db.itbsutah.com']
 
 var corsOptionsDelegate = function (req, callback) {
     var corsOptions;
@@ -21,6 +22,25 @@ app.use(express.json());
 app.use(cors(corsOptionsDelegate));
 
 app.use(express.static(path.join(__dirname, '/static/')));
+
+function database_() {
+    (this.get = async function (path) {
+        return '<h1>Under construction</h1>';
+    }),
+        (this.post = async function (path) {
+            return '<h1>Under construction</h1>';
+        })
+}
+
+const db = new database_();
+
+app.get('/kb/:id', async (req, res) => {
+    const id = req.params.id;
+
+    db.get(`/kb/${id}`).then(data => {
+        res.send(data)
+    })
+});
 
 const server = app.listen(2000, () => {
     console.log('ITBS Utah Support server is running');
