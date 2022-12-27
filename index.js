85d599f3b1a4470d98ff75e6fe678a18295209e5
Russell2259 <84256826+Russell2259@users.noreply.github.com>
2022-12-27 13:04:49 -0700

    added main structure

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { v4: uuid } = require('uuid');
+const sgMail = require('@sendgrid/mail');
+
+const whitelist = ['https://itbsutah.com', 'https://support.itbsutah.com']
+
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = { origin: true }
+    } else {
+        corsOptions = { origin: false }
+    }
+    callback(null, corsOptions)
+}
+
+const app = express();
+
+app.use(express.json());
+app.use(cors(corsOptionsDelegate));
+
+
+
+const server = app.listen(2000, () => {
+    console.log('ITBS Utah Support server is running');
+});
